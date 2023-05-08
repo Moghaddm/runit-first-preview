38c92f1616de6fdbc80508c408c9b84b8ff00991
Mohammad Mahdi Zeinolabedini Moghaddam <mahdimoghaddam2005@gmail.com>
2023-05-08 16:54:12 +0330

    Create DTO

---
+++ RunIt.WebApi/Application/Models/GameAddEditShowDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace RunIt.WebApi.Application.Models
+{
+    public class GameAddEditShowDTO
+    {
+            public string Title { get; set; }
+            public byte[] Image { get; set; }
+            public int Rate { get; set; }
+    }
+}
