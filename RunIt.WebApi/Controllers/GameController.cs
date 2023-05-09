b5884370fe5b34c615b4c11f4a4f32ee14bab6d7
moghaddam2005 <mahdimoghaddam2005@gmail.com>
2023-05-09 12:43:24 +0330

    Game Api Routes

--- RunIt.WebApi/Controllers/GameController.cs
+++ RunIt.WebApi/Controllers/GameController.cs
@@ -1,40 +1,48 @@
 using Microsoft.AspNetCore.Mvc;
 using RunIt.WebApi.Application;
-using RunIt.WebApi.Application.Model;
+using RunIt.WebApi.Application.Models;
 
 namespace RunIt.WebApi.Controllers;
 
 [ApiController]
-[Route("[controller]/[action]",Name = nameof(GameController))]
+[Route("[controller]/[action]", Name = nameof(GameController))]
 public class GameController : ControllerBase
 {
     private readonly IGameService _gameService;
-   public GameController(GameService Gameservice)
-   {
-   _gameService=Gameservice;
-   }
-    [HttpGet("[action]", Name = nameof(Get))]
-    public async ValueTask<IActionResult> Get() => Ok(_gameService.Get());
 
+    public GameController(IGameService gameservice) => _gameService = gameservice;
 
+    [HttpGet(Name = nameof(Get))]
+    public async ValueTask<IActionResult> Get() => Ok(await _gameService.Get());
 
-    [HttpGet("[action]/{Name}", Name = nameof(GetByName))]
-    public async ValueTask<IActionResult> GetByName(string Name)
+    [HttpGet(Name = nameof(GetByName))]
+    public async ValueTask<IActionResult> GetByName(string fullName)
     {
-        return  Ok(_gameService.GetByName(Name));
+        if (String.IsNullOrEmpty(fullName))
+            return BadRequest("Send Name of Game !");
+        return Ok(await _gameService.GetByName(fullName));
     }
 
+    public async ValueTask<IActionResult> GetById(int id)
+    {
+        var game = await _gameService.GetById(id);
+        if (game is not null)
+            return Ok(game);
+        return NotFound("Product Does Not Exist!");
+    }
 
-    [HttpGet("[action]", Name = nameof(DecreaseRate))]
-    public async ValueTask<IActionResult> DecreaseRate() => Ok(_gameService.DecreaseRate());
+    [HttpPatch("[action]", Name = nameof(DecreaseRate))]
+    public async ValueTask<IActionResult> DecreaseRate(int productId)
+    {
+        if (await _gameService.GetById(productId) is not null)
+            Ok(_gameService.DecreaseRate(productId));
+        return BadRequest("Send Product ID !");
+    }
 
-
-
-    [HttpGet("[action]", Name = nameof(GetCompanies))]
-    public async ValueTask<IActionResult> GetCompanies() => Ok(_gameService.GetCompanies);
-
-
-
-    [HttpGet("[action]", Name = nameof(IncreaseRate))]
-    public async ValueTask<IActionResult> IncreaseRate() => Ok(_gameService.IncreaseRate());
+    public async ValueTask<IActionResult> IncreaseRate(int productId)
+    {
+        if (await _gameService.GetById(productId) is not null)
+            Ok(_gameService.IncreaseRate(productId));
+        return BadRequest("Send Product ID !");
+    }
 }
